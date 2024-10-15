5d3dbf6c416e4c66a88d215cb1c844543be13531
charleskim <ssscool@naver.com>
2024-10-15 17:18:26 +0900

    firebase errer fix

--- src/index.js
+++ src/index.js
@@ -1,36 +1,8 @@
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getAuth, onAuthStateChanged } from "firebase/auth";
-// import { getFirestore, collection, getDocs, getDoc } from "firebase/firestore/lite";
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { collection, getDocs, getFirestore } from "firebase/firestore/lite";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD0W2E2lihwEvI6-4utdbCvVrflmQJC6-E",
-//   authDomain: "test001-a3974.firebaseapp.com",
-//   projectId: "test001-a3974",
-//   storageBucket: "test001-a3974.appspot.com",
-//   messagingSenderId: "795659655623",
-//   appId: "1:795659655623:web:d4d0358aca3998da8521e2",
-//   measurementId: "G-MMSS39XDVS",
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// const auth = getAuth(firebaseConfig);
-
-// onAuthStateChanged(auth, (user) => {
-//   if (user != null) {
-//     console.log("logged in!");
-//   } else {
-//     console.log("No user");
-//   }
-// });
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js'
-// Follow this pattern to import other Firebase services
-// import { } from 'firebase/<service>';
-
-// TODO: Replace the following with your app's Firebase project configuration
+// Firebase 프로젝트 설정
 const firebaseConfig = {
   apiKey: "AIzaSyD0W2E2lihwEvI6-4utdbCvVrflmQJC6-E",
   authDomain: "test001-a3974.firebaseapp.com",
@@ -41,13 +13,41 @@ const firebaseConfig = {
   measurementId: "G-MMSS39XDVS",
 };
 
+// Firebase 앱 초기화
+console.log("[INFO] Initializing Firebase app...");
 const app = initializeApp(firebaseConfig);
-const db = (app);
 
-// Get a list of cities from your database
-async function getCities(db) {
-  const citiesCol = collection(db, "cities");
-  const citySnapshot = await getDocs(citiesCol);
-  const cityList = citySnapshot.docs.map((doc) => doc.data());
-  return cityList;
+// Firestore 초기화
+console.log("[INFO] Initializing Firestore...");
+const db = getFirestore(app);
+
+// 인증 초기화
+console.log("[INFO] Initializing Firebase Authentication...");
+const auth = getAuth(app);
+
+// 로그인 상태 변화 감지
+console.log("[INFO] Checking authentication state...");
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log(`[INFO] User is logged in: ${user.email}`);
+  } else {
+    console.log("[INFO] No user is logged in");
+  }
+});
+
+// Firestore에서 도시 목록 가져오기
+async function getCities() {
+  console.log("[INFO] Fetching cities from Firestore...");
+  try {
+    const citiesCol = collection(db, "cities");
+    const citySnapshot = await getDocs(citiesCol);
+    const cityList = citySnapshot.docs.map((doc) => doc.data());
+    console.log("[INFO] City List:", cityList);
+    return cityList;
+  } catch (error) {
+    console.error("[ERROR] Error fetching cities:", error);
+  }
 }
+
+// Firestore에서 도시 목록 불러오기 테스트
+getCities();
