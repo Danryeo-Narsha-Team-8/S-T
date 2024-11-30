e4104d0b59eac05d21f35685c609e0b6ce4e061c
qpaseo <162771007+qpaseo@users.noreply.github.com>
2024-11-30 15:23:11 +0900

    fix : env
    env제거후 수정

--- src/config.js
+++ src/config.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyD0W2E2lihwEvI6-4utdbCvVrflmQJC6-E",
+  authDomain: "test001-a3974.firebaseapp.com",
+  projectId: "test001-a3974",
+  storageBucket: "test001-a3974.appspot.com",
+  messagingSenderId: "795659655623",
+  appId: "1:795659655623:web:d4d0358aca3998da8521e2",
+  measurementId: "G-MMSS39XDVS",
 };
 
 const app = initializeApp(firebaseConfig);
