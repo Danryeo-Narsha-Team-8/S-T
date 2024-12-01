f5430989626702fbfbc4ec8193a3136f5573bec4
qpaseo <162771007+qpaseo@users.noreply.github.com>
2024-12-01 12:41:25 +0900

    test : .env
    환경변수 받아오는 테스트

--- src/config.js
+++ src/config.js
@@ -3,6 +3,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 
+
+console.log("API Key:", process.env.REACT_APP_apiKey);
+
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
